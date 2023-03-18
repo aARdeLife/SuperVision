68bedc7cd8ed740868e2ff8581500334c78665af
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-03-18 22:06:09 +0100

    Update script.js

--- polaris/script.js
+++ polaris/script.js
@@ -1,13 +1,24 @@
-// Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
+AFRAME.registerComponent('tap-handler', {
+  init: function () {
+    this.el.addEventListener('click', (event) => {
+      // Get the location of the tap
+      const tapPosition = event.detail.intersection.point;
+
+      // Check if there is a stored location that matches the tap location
+      const match = this.findMatch(tapPosition);
+
+      // If there is a match, display the 3D model at the tap location
+      if (match) {
+        const model = document.createElement('a-entity');
+        model.setAttribute('gltf-model', 'path/to/model.gltf');
+        model.setAttribute('position', tapPosition);
+        this.el.sceneEl.appendChild(model);
+      }
+    });
+  },
+  findMatch: function (position) {
+    // Check if there is a stored location that matches the tap location
+    // This could be done using a database or by checking against a list of stored locations
+    return false;
   }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+});
